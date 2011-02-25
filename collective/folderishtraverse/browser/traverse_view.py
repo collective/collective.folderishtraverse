eb93ebae70fb0b12e8f76530fd61188241e46380
Johannes Raggam <raggam-nl@adm.at>
2011-02-25 14:18:04 +0100

    code cleanup, optimizations, removing unused tests

--- collective/folderishtraverse/browser/traverse_view.py
+++ collective/folderishtraverse/browser/traverse_view.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-# GNU General Public License (GPL)
-#
 from Products.Five.browser import BrowserView
 from plone.folder.interfaces import IFolder
 
@@ -18,14 +14,16 @@ class TraverseView(BrowserView):
     def __call__(self, *args, **kwargs):
         # TODO: only traverse to objects which are listed in typestolist.
         #       see adm.sfd.layout.browser.portlets.navigation.navtree_builder
-        if IFolder.providedBy(self.context) and self.anonymous:
-            if len(self.context.contentIds()):
+        ctx = self.context
+        if IFolder.providedBy(ctx) and self.anonymous:
+            con = ctx.contentIds()
+            if len(con):
                 # TODO: reversing the listing must be configurable!
-                obj = self.context.contentIds()[-1]
-                url = self.context[obj].absolute_url()
-                return self.request.response.redirect(url)
+                obj = con[-1]
+                url = ctx[obj].absolute_url()
 
-        # TODO: must this be a redirect? or can folder_summary_view retrieved
-        #       somehow else - like PageTemplateView, etc.?
-        url = self.context.absolute_url() + '/folder_summary_view'
+        if not url:
+            # TODO: must this be a redirect? or can folder_summary_view
+            #       retrieved somehow else - like PageTemplateView, etc.?
+            url = '%s/folder_summary_view' % ctx.absolute_url()
         return self.request.response.redirect(url)
