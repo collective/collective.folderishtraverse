4ac918baa074342f1cc07fbc8d948ab6f1266480
Johannes Raggam <raggam-nl@adm.at>
2013-04-17 13:57:25 +0200

    add merge-in i18n def file to not loose ZCML translations

--- build-i18n.sh
+++ build-i18n.sh
@@ -6,5 +6,5 @@ $I18NDUDE rebuild-pot --pot $I18NPATH/locales/$DOMAIN.pot --create $DOMAIN $I18N
 $I18NDUDE sync --pot $I18NPATH/locales/$DOMAIN.pot $I18NPATH/locales/*/LC_MESSAGES/$DOMAIN.po 
 
 DOMAIN=plone
-$I18NDUDE rebuild-pot --pot $I18NPATH/locales/$DOMAIN.pot --create $DOMAIN $I18NPATH
+$I18NDUDE rebuild-pot --pot $I18NPATH/locales/$DOMAIN.pot --merge $I18NPATH/locales/merge-plone.pot --create $DOMAIN $I18NPATH
 $I18NDUDE sync --pot $I18NPATH/locales/$DOMAIN.pot $I18NPATH/locales/*/LC_MESSAGES/$DOMAIN.po 
